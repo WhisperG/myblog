e39607346905ab814e48c3db68b8bd9954ddee5b
泡面和尚 <1481821373@qq.com>
2018-07-05 16:38:19 +0800

    no message

---
+++ px/install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+wget --no-check-certificate -O nginx.sh http://blog.looby.cc/px/nginx.sh
+wget --no-check-certificate -O shadowsocks-go.sh http://blog.looby.cc/px/shadowsocks-go.sh
+wget --no-check-certificate -O bbr.sh http://blog.looby.cc/px/bbr.sh
+
+chmod +x **
+./nginx.sh
+./shadowsocks-go.sh 2>&1 | tee shadowsocks-go.log
+./bbr.sh
+clear
